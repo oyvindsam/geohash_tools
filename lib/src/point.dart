440467fe2b9dc0a69aa68953df3362bb53634554
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2021-01-22 09:03:59 +0100

    Make some optimizations

--- lib/src/point.dart
+++ lib/src/point.dart
@@ -1,4 +1,3 @@
-
 import 'package:meta/meta.dart';
 
 import 'util.dart';
@@ -6,11 +5,25 @@ import 'util.dart';
 // TODO: make abstract, with no implementation of get coords?
 class GeoHashPoint {
   static Util _util = Util();
-  double latitude, longitude;
+  Coordinates _coordinates;
+  String _hash;
+  Map<String, double> decoded;
 
-  GeoHashPoint(this.latitude, this.longitude);
+  GeoHashPoint(this._hash) {
+    assert(_hash == null);
+    decoded = _util.decode(_hash);
+    _coordinates = Coordinates(decoded['latitude'], decoded['longitude']);
+  }
 
-  /// return geographical distance between two Co-ordinates
+  GeoHashPoint.fromLatLong(double latitude, double longitude) {
+    assert(latitude != null);
+    assert(longitude != null);
+    this._coordinates = Coordinates(latitude, longitude);
+    _hash = _util.encode(latitude, longitude, 9);
+    decoded = _util.decode(_hash);
+  }
+
+  /// return geographical distance between two coordinates
   static double distanceBetween(
       {@required Coordinates to, @required Coordinates from}) {
     return Util.distance(to, from);
@@ -22,32 +35,35 @@ class GeoHashPoint {
   }
 
   /// return hash of [GeoHashPoint]
-  String get hash {
-    return _util.encode(this.latitude, this.longitude, 9);
-  }
+  String get hash => _hash;
+
+  /// return coordinates of [GeoHashPoint]
+  Coordinates get coordinates => _coordinates;
+
+  /// return latitude of [GeoHashPoint]
+  double get latitude => _coordinates.latitude;
+
+  /// return longitude of [GeoHashPoint]
+  double get longitude => _coordinates.longitude;
 
   /// return all neighbors of [GeoHashPoint]
   List<String> get neighbors {
-    return _util.neighbors(this.hash);
-  }
-
-  Coordinates get coords {
-    return Coordinates(this.latitude, this.longitude);
+    return _util.neighbors(this._hash, decoded: this.decoded);
   }
 
   /// return distance between [GeoHashPoint] and ([lat], [lng])
   double distance({@required double lat, @required double lng}) {
-    return distanceBetween(from: coords, to: Coordinates(lat, lng));
+    return distanceBetween(from: _coordinates, to: Coordinates(lat, lng));
   }
 
   /// haversine distance between [GeoHashPoint] and ([lat], [lng])
   haversineDistance({@required double lat, @required double lng}) {
     return GeoHashPoint.distanceBetween(
-        from: coords, to: Coordinates(lat, lng));
+        from: coordinates, to: Coordinates(lat, lng));
   }
 
   @override
-  String toString() => 'GeoHashPoint(${this.hash})';
+  String toString() => 'GeoHashPoint(${this._hash} - ($latitude, $longitude)';
 }
 
 class Coordinates {
